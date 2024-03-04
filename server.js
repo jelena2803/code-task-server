2e31774dd160b3ec31201e020c4f24b3792b5a66
jelena2803 <jelenaiva2803@gmail.com>
2024-03-04 19:45:08 +0200

    changes made with api middleware

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-
+require("dotenv").config();
 const app = express();
 const PORT = 8080;
 
+function middleware(req, res, next) {
+  const testApiValue = process.env.TEST_API;
+  // Use testApiValue in your middleware logic
+  console.log("TEST_API value:", testApiValue);
+  next();
+}
+
 app.use(cors());
+app.use(customMiddleware);
 
 app.get("/houses", async (req, res) => {
   const { name } = req.query;
