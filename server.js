3cb4ac3cc080d132cb19f63afa9d0f22e03712ff
jelena2803 <jelenaiva2803@gmail.com>
2024-03-04 20:57:21 +0200

    added port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const axios = require("axios");
 require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080 || 3000;
 const baseURL = process.env.VERCEL_URL || `http://localhost:8080`;
 
 function middleware(req, res, next) {
